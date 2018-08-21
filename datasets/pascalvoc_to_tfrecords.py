6a9bdbe2416fbace4f8ee5fc2942545300e16e92
zm <4698805@163.com>
2018-08-21 15:23:46 +0800

    modified pascalvoc_totfrecords, transform data from wider_train directly without voc format

--- datasets/pascalvoc_to_tfrecords.py
+++ datasets/pascalvoc_to_tfrecords.py
@@ -33,84 +33,74 @@ serialized Example proto. The Example proto contains the following fields:
 Note that the length of xmin is identical to the length of xmax, ymin and ymax
 for each example.
 """
+import math
 import os
-import sys
 import random
 
-import numpy as np
+import cv2
 import tensorflow as tf
 
-import xml.etree.ElementTree as ET
-
-from dataset_utils import int64_feature, float_feature, bytes_feature
-from pascalvoc_datasets import VOC_LABELS
-import math
-
-DIRECTORY_ANNOTATIONS = 'Annotations/'
-DIRECTORY_IMAGES = 'JPEGImages/'
+from datasets.dataset_utils import int64_feature, float_feature, bytes_feature
 
 
-def _process_image(directory, name):
-    """Process a image and annotation file.
+def _process_image(info,dataset_path):
+    '''
 
-    Args:
-      filename: string, path to an image file e.g., '/path/to/example.JPG'.
-      coder: instance of ImageCoder to provide TensorFlow image coding utils.
-    Returns:
-      image_buffer: string, JPEG encoding of RGB image.
-      height: integer, image height in pixels.
-      width: integer, image width in pixels.
-    """
-    # Read the image file.
+    :param directory: image directory
+    :param name: image name
+    :return:
+    '''
     #filename = directory + DIRECTORY_IMAGES + name + '.jpg'
-    filename = os.path.join(directory, DIRECTORY_IMAGES, '{}.jpg'.format(name))
+    filename = os.path.join(dataset_path, info[0] + '.jpg')
     image_data = tf.gfile.FastGFile(filename, 'rb').read()
-
-    # Read the XML annotation file.
-    filename = os.path.join(directory, DIRECTORY_ANNOTATIONS, name + '.xml')
-    tree = ET.parse(filename)
-    root = tree.getroot()
-
-    # Image shape.
-    size = root.find('size')
-    shape = [int(size.find('height').text),
-             int(size.find('width').text),
-             int(size.find('depth').text)]
+    img = cv2.imread(filename)
+    shape = img.shape
+    #print (shape)
+    data_example = dict()
+    data_example['name'] = filename
+    data_example['image'] = image_data
+    data_example['shape'] = shape
+    #print(info[1])
     # Find annotations.
     bboxes = []
-    labels = []
-    labels_text = []
-    difficult = []
-    truncated = []
-    for obj in root.findall('object'):
-        label = obj.find('name').text
-        labels.append(int(VOC_LABELS[label][0]))
-        if(int(VOC_LABELS[label][0]) == 0):
-            print(filename)
-            raise 
-        
-        labels_text.append(label.encode('ascii'))
-
-        if obj.find('difficult'):
-            difficult.append(int(obj.find('difficult').text))
-        else:
-            difficult.append(0)
-        if obj.find('truncated'):
-            truncated.append(int(obj.find('truncated').text))
-        else:
-            truncated.append(0)
-
-        bbox = obj.find('bndbox')
-        bboxes.append((float(bbox.find('ymin').text) / shape[0],
-                       float(bbox.find('xmin').text) / shape[1],
-                       float(bbox.find('ymax').text) / shape[0],
-                       float(bbox.find('xmax').text) / shape[1]
-                       ))
-    return image_data, shape, bboxes, labels, labels_text, difficult, truncated
+    bbox = dict()
 
 
-def _convert_to_example(image_data, labels, labels_text, bboxes, shape,
-                        difficult, truncated,name):
+    for i in range(1,len(info)-1):
+
+        info[i] = float(info[i])
+        assert  isinstance(info[i],float)
+        if i%4 == 1:
+
+            bbox['xmin'] = info[i]
+        if i%4 == 2:
+            bbox['ymin'] = info[i]
+        if i%4 == 3:
+            bbox['xmax'] = info[i]
+        if i%4 == 0:
+            bbox['ymax'] = info[i]
+            bboxes.append(bbox)
+    #print('filename: %s, bboxes: %s' %(filename,bboxes))
+    data_example['bboxes'] = bboxes
+
+    '''
+        for bbox in bboxes:
+        cv2.rectangle(img,
+                      (int(float(bbox['xmin'])),int(float(bbox['ymin']))),
+                      (int(float(bbox['xmax'])),int(float(bbox['ymax']))),
+                      (0,0,255))
+        cv2.imshow('lala',img)
+        cv2.waitKey(0)
+
+    '''
+
+
+
+
+    return data_example
+
+
+def _convert_to_example(data_example):
     """Build an Example proto for an image example.
 
     Args:
@@ -128,10 +118,18 @@ def _convert_to_example(image_data, labels, labels_text, bboxes, shape,
     ymin = []
     xmax = []
     ymax = []
-    for b in bboxes:
-        assert len(b) == 4
+    shape= []
+    for s in data_example['shape']:
+        shape.append(s)
+    image_data = data_example['image']
+    filename = data_example['name']
+    for bbox in data_example['bboxes']:
+        assert len(bbox) == 4
         # pylint: disable=expression-not-assigned
-        [l.append(point) for l, point in zip([ymin, xmin, ymax, xmax], b)]
+        xmin.append(bbox['xmin'])
+        ymin.append(bbox['ymin'])
+        xmax.append(bbox['xmax'])
+        ymax.append(bbox['ymax'])
         # pylint: enable=expression-not-assigned
 
     image_format = b'JPEG'
@@ -144,17 +142,13 @@ def _convert_to_example(image_data, labels, labels_text, bboxes, shape,
             'image/object/bbox/xmax': float_feature(xmax),
             'image/object/bbox/ymin': float_feature(ymin),
             'image/object/bbox/ymax': float_feature(ymax),
-            'image/object/bbox/label': int64_feature(labels),
-            'image/object/bbox/label_text': bytes_feature(labels_text),
-            'image/object/bbox/difficult': int64_feature(difficult),
-            'image/object/bbox/truncated': int64_feature(truncated),
             'image/format': bytes_feature(image_format),
-            'image/filename': bytes_feature(name.encode('utf-8')),
+            'image/filename': bytes_feature(filename.encode('utf-8')),
             'image/encoded': bytes_feature(image_data)}))
     return example
 
 
-def _add_to_tfrecord(dataset_dir, name, tfrecord_writer):
+def _add_to_tfrecord(info,dataset_path, tfrecord_writer):
     """Loads data from image and annotations files and add them to a TFRecord.
 
     Args:
@@ -162,70 +156,86 @@ def _add_to_tfrecord(dataset_dir, name, tfrecord_writer):
       name: Image name to add to the TFRecord;
       tfrecord_writer: The TFRecord writer to use for writing.
     """
-    image_data, shape, bboxes, labels, labels_text, difficult, truncated = \
-        _process_image(dataset_dir, name)
-    example = _convert_to_example(image_data, labels, labels_text,
-                                  bboxes, shape, difficult, truncated,name)
+    data_example = \
+        _process_image(info,dataset_path)
+    example = _convert_to_example(data_example)
     tfrecord_writer.write(example.SerializeToString())
 
 
 def _get_output_filename(output_dir, name):
     return '%s/%s.tfrecord' % (output_dir, name)
 
-def _get_dataset_filename(dataset_dir, name, shard_id, num_shard, records_num):
+def _get_dataset_filename(output_dir, name, shard_id, num_shard, records_num):
     output_filename = '%s_%05d-of-%05d-total%05d.tfrecord' % (name, shard_id + 1, num_shard,records_num)
-    return os.path.join(dataset_dir, output_filename)
+    return os.path.join(output_dir, output_filename)
 
-def run(dataset_dir, output_dir, name, shuffling=False):
-    """Runs the conversion operation.
+def run(anno_path,dataset_path, output_dir, name, shuffling=False):
 
-    Args:
-      dataset_dir: The dataset directory where the dataset is stored.
-      output_dir: Output directory.
-    """
-    
 
     # Dataset filenames, and shuffling.
-    path = os.path.join(dataset_dir, DIRECTORY_ANNOTATIONS)
-    if not tf.gfile.Exists(path):
-        raise Exception("{} does not exist".format(path))
+
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
-    filenames = sorted(os.listdir(path))
+    annotations = get_annotations(anno_path)
+
     if shuffling:
         random.seed(12345)
-        random.shuffle(filenames)
+        random.shuffle(annotations)
 
     # Process dataset files.
     num_per_shard = 200
-    num_shard = int(math.ceil(len(filenames) / float(num_per_shard)))
+    num_shard = int(math.ceil(len(annotations) / float(num_per_shard)))
     
     for shard_id in range(num_shard):
         start_ndx = shard_id * num_per_shard
-        end_ndx = min((shard_id+1) * num_per_shard, len(filenames))
+        end_ndx = min((shard_id+1) * num_per_shard, len(annotations))
         records_num = end_ndx - start_ndx
         tf_filename = _get_dataset_filename(output_dir, name, shard_id, num_shard, records_num)
         with tf.python_io.TFRecordWriter(tf_filename) as tfrecord_writer:
             for i in range(start_ndx, end_ndx):
-                filename = filenames[i]
+                info = annotations[i].split(' ')
+                filename = os.path.join(dataset_path, info[0] + '.jpg')
+
+
+                _add_to_tfrecord(info, dataset_path,tfrecord_writer)
+
+
                 
-                print('Converting image %d/%d %s shard %d' % (i+1, len(filenames), filename[:-4], shard_id+1))
+                print('Converting image %d/%d %s shard %d' % (i+1, len(annotations), filename, shard_id+1))
                 #save the file to tfrecords
-                _add_to_tfrecord(dataset_dir, filename[:-4], tfrecord_writer)
+                '''
+                print(filename)
+                img = cv2.imread(filename + '.jpg')
+                cv2.imshow(filename, img)
+                cv2.waitKey(0)
+               
+                '''
+
+
 
     # Finally, write the labels file:
     # labels_to_class_names = dict(zip(range(len(_CLASS_NAMES)), _CLASS_NAMES))
     # dataset_utils.write_label_file(labels_to_class_names, dataset_dir)
-    print('\nFinished converting the Pascal VOC dataset!')
+    print('\nFinished converting the Widerface dataset!')
     
-    
+def get_annotations(anno_path):
+    anno_file = open(anno_path,'r')
+    annotations = anno_file.readlines()
+    return annotations
+
+
+
+
 if __name__ == "__main__": 
     
-    dataset_dir = 'datasets/widerface/'
-    output_dir = "tfrecords/"
-    name='voc_train_2007'
+    dataset_dir = '../../DATA/WIDER_train/images/'
+    anno_file = 'wider_face_train.txt'
+    output_dir = "../../DATA/wider_tfrecord/"
+    name='Wider_Face'
+
+    #get_filenames(anno_file,dataset_dir)
     
-    run(dataset_dir, output_dir, name=name, shuffling=True)
+    run(anno_file,dataset_dir, output_dir, name=name, shuffling=False)
     
     
     
